1129101e51a80c076599e8a9565223be70cbffa0
jnassula <jonatanassula@gmail.com>
2023-06-03 12:54:08 +0100

    reorganizing files

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id: int):
+    return render(request, 'recipes/pages/recipe-view.html')
