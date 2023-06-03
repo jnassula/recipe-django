17bed54126c10017e2ae67edfdc8ccafc581aac3
jnassula <jonatanassula@gmail.com>
2023-06-03 16:00:36 +0100

    injecting data into recipe html

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(6)],
+        })
 
 
 def recipe(request, id: int):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        })
