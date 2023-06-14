a104a8c423baf3ffe5873f7c84c3a55daf5e95b9
jnassula <jonatanassula@gmail.com>
2023-06-14 11:00:37 +0100

    category title has been updated

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+            is_published=True
+            ).order_by('-id')
     return render(
         request,
         'recipes/pages/home.html',
@@ -13,12 +15,16 @@ def home(request):
     )
 
 def category(request, category_id: int):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    recipes = Recipe.objects.filter(
+            category__id=category_id, 
+            is_published=True
+            ).order_by('-id')
     return render(
         request,
-        'recipes/pages/home.html',
+        'recipes/pages/category.html',
         context={
             'recipes': recipes,
+            'title': f'{recipes.first().category.name} - Category | '
         },
     )
 
