b6a54c02e5cc37fb8f3e65675c273ed99d460c4e
jnassula <jonatanassula@gmail.com>
2023-06-14 11:06:44 +0100

    migrations

--- recipes/migrations/0001_initial.py
+++ recipes/migrations/0001_initial.py
@@ -14,47 +14,50 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="Category",
+            name='Category',
             fields=[
                 (
-                    "id",
+                    'id',
                     models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
-                        verbose_name="ID",
+                        verbose_name='ID',
                     ),
                 ),
-                ("name", models.CharField(max_length=65)),
+                ('name', models.CharField(max_length=65)),
             ],
         ),
         migrations.CreateModel(
-            name="Recipe",
+            name='Recipe',
             fields=[
                 (
-                    "id",
+                    'id',
                     models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
-                        verbose_name="ID",
+                        verbose_name='ID',
                     ),
                 ),
-                ("title", models.CharField(max_length=65)),
-                ("description", models.CharField(max_length=165)),
-                ("slug", models.SlugField()),
-                ("preparation_time", models.IntegerField()),
-                ("preparation_time_unit", models.CharField(max_length=65)),
-                ("servings", models.IntegerField()),
-                ("servings_unit", models.CharField(max_length=65)),
-                ("preparation_steps", models.TextField()),
-                ("preparation_steps_is_html", models.BooleanField()),
-                ("created_at", models.DateTimeField(auto_now_add=True)),
-                ("updated_at", models.DateTimeField(auto_now=True)),
-                ("is_public", models.BooleanField()),
-                ("cover", models.ImageField(upload_to="recipes/covers/%Y/%m/%d")),
+                ('title', models.CharField(max_length=65)),
+                ('description', models.CharField(max_length=165)),
+                ('slug', models.SlugField()),
+                ('preparation_time', models.IntegerField()),
+                ('preparation_time_unit', models.CharField(max_length=65)),
+                ('servings', models.IntegerField()),
+                ('servings_unit', models.CharField(max_length=65)),
+                ('preparation_steps', models.TextField()),
+                ('preparation_steps_is_html', models.BooleanField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('is_public', models.BooleanField()),
                 (
-                    "author",
+                    'cover',
+                    models.ImageField(upload_to='recipes/covers/%Y/%m/%d'),
+                ),
+                (
+                    'author',
                     models.ForeignKey(
                         null=True,
                         on_delete=django.db.models.deletion.SET_NULL,
@@ -62,11 +65,11 @@ class Migration(migrations.Migration):
                     ),
                 ),
                 (
-                    "category",
+                    'category',
                     models.ForeignKey(
                         null=True,
                         on_delete=django.db.models.deletion.SET_NULL,
-                        to="recipes.category",
+                        to='recipes.category',
                     ),
                 ),
             ],
