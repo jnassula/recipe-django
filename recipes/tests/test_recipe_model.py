a7848eb99a26f8dd0ca63233c85576b3e00444b8
jnassula <jonatanassula@gmail.com>
2023-07-03 12:28:39 +0100

    test for recipe title if title is longer than 65 chars has been created

--- recipes/tests/test_recipe_model.py
+++ recipes/tests/test_recipe_model.py
@@ -1,4 +1,5 @@
 from .test_recipe_base import RecipeBaseTest
+from django.core.exceptions import ValidationError
 
 
 class RecipeModelTest(RecipeBaseTest):
@@ -7,4 +8,7 @@ class RecipeModelTest(RecipeBaseTest):
         return super().setUp()
 
     def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
-        self.fail()
+        self.recipe.title = "A" * 70
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
